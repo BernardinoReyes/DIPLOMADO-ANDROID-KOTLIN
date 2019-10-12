4b526a48cbfb9b721323ca6f84f11cc7489210f5
BernardinoReyes <Bernardino.Reyes.J@gmail.com>
2019-10-12 19:25:22 -0400

    Add files via upload

---
+++ Practica 02 Kotlin CodeLabs/Aboutme_Bernardino/app/src/main/java/com/diplomado/aboutme_bernardino/MainActivity_Aboutme.kt
@@ -0,0 +1,12 @@
+package com.diplomado.aboutme_bernardino
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity_Aboutme : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_aboutme)
+    }
+}
